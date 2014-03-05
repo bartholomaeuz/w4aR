34373e969fe99463b939ecad10c54fd41a7b8536
bartholomaeuz <bartholomaeuz@github.com>
2014-03-05 20:14:56 +0100

    xml2

---
+++ paper/2014/foss4g-e/foot-osrm.lua
@@ -0,0 +1,30 @@
+walking_speed = 5
+
+speeds = {
+  ["primary"] = walking_speed,
+  ["primary_link"] = walking_speed,
+  ["secondary"] = walking_speed,
+  ["secondary_link"] = walking_speed,
+  ["tertiary"] = walking_speed,
+  ["tertiary_link"] = walking_speed,
+  ["unclassified"] = walking_speed,
+  ["residential"] = walking_speed,
+  ["road"] = walking_speed,
+  ["living_street"] = walking_speed,
+  ["service"] = walking_speed,
+  ["track"] = walking_speed,
+  ["path"] = walking_speed,
+  ["steps"] = walking_speed,
+  ["pedestrian"] = walking_speed,
+  ["footway"] = walking_speed,
+  ["pier"] = walking_speed,
+  ["default"] = walking_speed
+}
+
+surface_speeds = {
+	["fine_gravel"] =   walking_speed*0.75,
+	["gravel"] =        walking_speed*0.75,
+	["pebbelstone"] =   walking_speed*0.75,
+	["mud"] =           walking_speed*0.5,
+	["sand"] =          walking_speed*0.5
+}
