140b2a96ce606e143d6d45775329d6f3fadb9c78
unknown <Dornh@KPROJM5963.technikum.fh-joanneum.local>
2014-03-10 11:26:08 +0100

    otp2

--- paper/2014/foss4g-e/foot-osrm.lua
+++ paper/2014/foss4g-e/foot-osrm.lua
@@ -1,20 +1,13 @@
 walking_speed = 5
 
 speeds = {
-  ["primary"] = walking_speed,
-  ["primary_link"] = walking_speed,
-  ["secondary"] = walking_speed,
-  ["secondary_link"] = walking_speed,
-  ["tertiary"] = walking_speed,
-  ["tertiary_link"] = walking_speed,
-  ["unclassified"] = walking_speed,
-  ["residential"] = walking_speed,
-  ["road"] = walking_speed,
+  ["residential"] = walking_speed*0.5,
+  ["road"] = walking_speed*0.5,
   ["living_street"] = walking_speed,
-  ["service"] = walking_speed,
-  ["track"] = walking_speed,
-  ["path"] = walking_speed,
-  ["steps"] = walking_speed,
+  ["service"] = walking_speed*0.6,
+  ["track"] = walking_speed*0.8,
+  ["path"] = walking_speed*0.8,
+  ["steps"] = walking_speed*0.5,
   ["pedestrian"] = walking_speed,
   ["footway"] = walking_speed,
   ["pier"] = walking_speed,
@@ -22,9 +15,9 @@ speeds = {
 }
 
 surface_speeds = {
-	["fine_gravel"] =   walking_speed*0.75,
-	["gravel"] =        walking_speed*0.75,
-	["pebbelstone"] =   walking_speed*0.75,
-	["mud"] =           walking_speed*0.5,
-	["sand"] =          walking_speed*0.5
+  ["fine_gravel"] =   walking_speed*0.75,
+  ["gravel"] =        walking_speed*0.75,
+  ["pebbelstone"] =   walking_speed*0.75,
+  ["mud"] =           walking_speed*0.5,
+  ["sand"] =          walking_speed*0.5
 }
