54fe91a45fdbd6988e5d04b1346cf475833f67e1
unknown <Dornh@KPROJM5963.technikum.fh-joanneum.local>
2014-03-11 14:59:50 +0100

    c1

--- paper/2014/foss4g-e/foot-osrm.lua
+++ paper/2014/foss4g-e/foot-osrm.lua
@@ -1,23 +1,17 @@
 walking_speed = 5
 
 speeds = {
-  ["residential"] = walking_speed*0.5,
-  ["road"] = walking_speed*0.5,
-  ["living_street"] = walking_speed,
-  ["service"] = walking_speed*0.6,
+  ["residential"] = walking_speed*0.5, 
+  ["living_street"] = walking_speed, 
   ["track"] = walking_speed*0.8,
   ["path"] = walking_speed*0.8,
   ["steps"] = walking_speed*0.5,
   ["pedestrian"] = walking_speed,
-  ["footway"] = walking_speed,
-  ["pier"] = walking_speed,
-  ["default"] = walking_speed
+  ["footway"] = walking_speed
 }
 
 surface_speeds = {
   ["fine_gravel"] =   walking_speed*0.75,
-  ["gravel"] =        walking_speed*0.75,
-  ["pebbelstone"] =   walking_speed*0.75,
-  ["mud"] =           walking_speed*0.5,
+  ["gravel"] =        walking_speed*0.75,  
   ["sand"] =          walking_speed*0.5
 }
