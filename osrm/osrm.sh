8c060500140256a2c2f36d2c2c67442a893e57d7
unknown <Dornh@KPROJM5963.technikum.fh-joanneum.local>
2014-04-03 10:34:52 +0200

    new osrm

--- osrm/osrm.sh
+++ osrm/osrm.sh
@@ -2,24 +2,34 @@
 # mado@hamina.fh-joanneum.at (PW:w6s46KB)
 # !!!!2.Danach deployen!!!!
 # mado@malta.fh-joanneum.at (PW:w6s46KB)
+# cd Project-OSRM
 # run: sudo osrm.sh 
 # besser Kommandos einzeln mit SUDO ausführen
- 
-echo "### START"
-#cd Project-OSRM
+
+echo "### DELETE: Austria OSM"
+rm austria-latest.osm.pbf 
 echo "### DOWNLOAD: Austria OSM"
 wget http://download.geofabrik.de/europe/austria-latest.osm.pbf
-echo "###KILL OLD OSRM PROCESS"
+echo "###LIST OSRM PROCESS (Malta)"
+pgrep osrm
+echo "###KILL OLD OSRM PROCESS (Malta)"
 pkill osrm
-echo "### EXTRACT DATA (WAIT approx. 10 min)"
+echo "### EXTRACT DATA (WAIT approx. 15 min)"
 ./osrm-extract austria-latest.osm.pbf
-echo "### PREPARE DATA (WAIT approx. 1 h 15 min)"
+echo "### PREPARE DATA (WAIT approx. 1h 15 min)"
 ./osrm-prepare austria-latest.osrm
-# date +%s > austria-latest.osrm.timestamp (needed 1x)
-echo "### RUN OSRM PROCESS"
+# date +%s > austria-latest.osrm.timestamp (once needed)
+echo "### RUN OSRM PROCESS (in background &)"
 ./osrm-routed &
 
-# OSRM Files können auch von HAMINA auf MALTA kopiert werden
-scp austria-latest.osrm* mado@malta:/home/mado/Project-OSRM
+# OSRM Files sollten von HAMINA auf MALTA kopiert werden
+# pkill osrm    # @malta
+# scp austria-latest.osrm* mado@malta:/home/mado/Project-OSRM
+# sudo ./osrm-routed &   # @malta
 
-# check links on http://hamina/mado and http://malta/mado
+# check links
+# http://hamina.fh-joanneum.at/mado
+# http://malta.fh-joanneum.at/mado
+
+# Profile (profile.lua) -> Priority of roads, naming schema
+# Server Settings (server.ini) -> Files, Port 
