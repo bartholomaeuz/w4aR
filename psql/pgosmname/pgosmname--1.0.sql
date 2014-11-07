1583122f9abc1b7631b71ed388daa516b4a70738
unknown <dornh@KPROJM5963.technikum.fh-joanneum.local>
2014-11-07 11:50:29 +0100

    small changes 2

--- psql/pgosmname/pgosmname--1.0.sql
+++ psql/pgosmname/pgosmname--1.0.sql
@@ -1,6 +1,9 @@
 -- pg osm name EXTENSION 
 create index ON_name_idx on planet_osm_point using gist (name gist_trgm_ops);
 
+create or replace function ON_test_place_3(text) returns setof planet_osm_point as $$          
+ select * from planet_osm_point where name % $1 order by similarity(name,$1) desc;
+ $$ LANGUAGE SQL;
 
 create  or replace function ON_test_place_2(text) returns table(place text) as $$
  select name from planet_osm_point where place is not null and name % $1;
