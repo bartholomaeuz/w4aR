9479e20c8859ab172d4cba63e24b4028174b2a82
unknown <dornh@KPROJM5963.technikum.fh-joanneum.local>
2014-11-07 14:03:00 +0100

    new place

--- psql/pgosmname/pgosmname--1.0.sql
+++ psql/pgosmname/pgosmname--1.0.sql
@@ -2,7 +2,7 @@
 create index ON_name_idx on planet_osm_point using gist (name gist_trgm_ops);
 
 create or replace function ON_test_place_3(text) returns setof planet_osm_point as $$          
- select * from planet_osm_point where name % $1 order by similarity(name,$1) desc;
+ select * from planet_osm_point where place is not null and name % $1 order by similarity(name,$1) desc;
  $$ LANGUAGE SQL;
 
 create  or replace function ON_test_place_2(text) returns table(place text) as $$
