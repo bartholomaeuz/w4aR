39daaccff78affd5f3af9a8be584a316e4732805
unknown <dornh@KPROJM5963.technikum.fh-joanneum.local>
2014-11-07 11:31:08 +0100

    f test place 1 bug

--- psql/pgosmname/pgosmname--1.0.sql
+++ psql/pgosmname/pgosmname--1.0.sql
@@ -5,6 +5,6 @@ create function ON_top10(text) returns table(ort text,sim real) as $$
  select distinct name,similarity(name,$1) from planet_osm_point where name % $1 order by similarity(name,$1) desc limit 10;
  $$ LANGUAGE SQL;
  
-create function ON_test_place_1(text) returns table(ort textq) as $$
+create function ON_test_place_1(text) returns table(place text) as $$
  select name from planet_osm_point where place is not null and name % $1;
  $$ LANGUAGE SQL;
